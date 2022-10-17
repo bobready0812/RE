a14e094c0f8d70fb2fff74f52b015d5f2e11aa5e
DaehyeonKim <Daehyeonoff@gmail.com>
2022-10-17 23:34:46 +0900

    20221017

--- type/src/App.js
+++ type/src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 
 function App() {
   const [value,setValue] = useState("");
+  const handleChange = (e) => {
+    const value = e.target.value;
+    console.log(value);
+  }
 
   return (
     <div>
-<input></input>
+<input value={value} onChange={handleChange}></input>
     </div>
   );
 }
