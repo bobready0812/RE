639a1f1d7e2f9f8eb742d150d014219892b48196
DaehyeonKim <Daehyeonoff@gmail.com>
2022-10-21 22:44:45 +0900

    20221011

--- type/src/App.js
+++ type/src/App.js
@@ -2,16 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [value,setValue] = useState("");
-  const handleChange = (e) => {
-    setValue(e.target.value);
-    
-  }
-
-  return (
-    <div>
-<input value={value} onChange={handleChange}></input>
-    </div>
+ 
   );
 }
 
